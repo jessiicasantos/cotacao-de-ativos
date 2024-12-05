c7535f5fa04d0126c9af18fd431228798fcc4c7f
Jessica Santos <jessiicansantos@gmail.com>
2024-12-05 13:34:53 -0300

    Added Shadcn table

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Navbar />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,48 @@
-const Home = () => (
-    <div>
-        <h1>Home</h1>
-    </div>
-)
+import { useEffect, useState } from "react";
+import { columns, Payment } from "../components/ui/payments/columns";
+import { DataTable } from "../components/ui/payments/data-table";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "234546df",
+      amount: 200,
+      status: "pending",
+      email: "mdaf@example.com.br",
+    }
+    // ...
+  ]
+};
+
+const Home = () => {
+    const [data, setData] = useState<any>([]);
+
+    async function fetchData() {
+        try {
+          let response = await getData();
+
+          setData(response);
+        } catch(error) {
+            console.error(`Errooo!!!\n ${error}`)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+  return (
+    <>
+      <DataTable columns={columns} data={data} />
+    </>
+  )
+};
 
 export default Home;
