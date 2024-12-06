725ceb176f636523e7693ad330429850f4f40d38
Jessica Santos <jessiicansantos@gmail.com>
2024-12-06 18:11:06 -0300

    Added mobile to the Login page & type to the Buttons

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,14 +1,14 @@
-export const ButtonGreen = ({ children, className }: any) => {
+export const ButtonGreen = ({ children, className, type }: any) => {
     return (
-        <button className={`${className} bg-[#37776C] hover:bg-gray-900 text-white font-bold py-2 px-4 rounded`}>
+        <button type={type} className={`${className} bg-[#37776C] hover:bg-gray-900 text-white font-bold py-2 px-4 rounded`}>
             {children}
         </button>
     )
 }
 
-export const ButtonBorder = ({ children, className }: any) => {
+export const ButtonBorder = ({ children, className, type }: any) => {
     return (
-        <button className={`${className} bg-white text-gray-600 shadow-[0_0_2px_rgba(0,0,0,0.5)] hover:bg-red-600 hover:text-white font-bold py-2 px-4 rounded`}>
+        <button type={type} className={`${className} bg-white text-gray-600 shadow-[0_0_2px_rgba(0,0,0,0.5)] hover:bg-red-600 hover:text-white font-bold py-2 px-4 rounded`}>
             {children}
         </button>
     )
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,88 +1,73 @@
+import { ButtonGreen } from "../components/Buttons";
 import { Link } from "react-router-dom";
+import OptionsCompany from "./../assets/img/options-company-logo.png";
 
 export function Login() {
     return (
-      <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-              className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <Link to="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </Link>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm/6 text-gray-500">
-              Not a member?{' '}
-              <Link to="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </Link>
-            </p>
-          </div>
+      <div className="h-[calc(100vh-64px)] flex flex-col lg:flex-row justify-between align-center">
+        <div className="flex flex-col align-center justify-center bg-[#37776C] h-full m-auto p-3.5 md:p-12 w-full lg:w-1/2">
+          <img
+            alt="Options & Company"
+            src={OptionsCompany}
+            className="mx-auto lg:mb-12 h-10 w-64 h-24 object-contain"
+          />
         </div>
-      </>
+
+        <div className="flex flex-col align-center justify-center p-3.5 md:p-12 w-full lg:w-1/2">
+          <form action="#" method="POST" className="space-y-6 sm:w-full sm:max-w-sm sm:m-auto">
+            <div>
+                <h2 className="pb-2 text-left text-2xl/9 font-bold text-gray-900">
+                  Login
+                </h2>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  placeholder="E-mail*"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <div className="text-sm">
+                  <Link to="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Esqueci minha senha
+                  </Link>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  placeholder="Senha*"
+                />
+              </div>
+            </div>
+
+            <div>
+              <ButtonGreen type="submit" className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Sign in
+              </ButtonGreen>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Novo por aqui?{' '}
+            <Link to="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Crie sua conta!
+            </Link>
+          </p>
+        </div>
+      </div>
     )
   }
 
